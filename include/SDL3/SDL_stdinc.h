927324b4064dafe9a1bdf7afff299f28b59cb786
Shay <shabennett2@gmail.com>
2026-04-07 08:13:34 -0600

    Replace `POWERPC` with `ppc` macro

--- include/SDL3/SDL_stdinc.h
+++ include/SDL3/SDL_stdinc.h
@@ -1166,7 +1166,7 @@ SDL_COMPILE_TIME_ASSERT(longlong_size64, sizeof(long long) == 8); /* using I64 f
 
 /** \cond */
 #ifndef DOXYGEN_SHOULD_IGNORE_THIS
-#if !(defined(__APPLE__) && defined(__POWERPC__))
+#if !(defined(__APPLE__) && defined(__ppc__))
 SDL_COMPILE_TIME_ASSERT(bool_size, sizeof(bool) == 1);
 #endif
 SDL_COMPILE_TIME_ASSERT(uint8_size, sizeof(Uint8) == 1);
